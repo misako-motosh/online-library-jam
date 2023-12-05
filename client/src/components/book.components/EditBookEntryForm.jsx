72c573d173a07b53520a806f8501c0c5724429f6
daomingley73 <mmotosh@yahoo.com>
2023-12-05 17:36:05 +0800

    feat: UserBookList, AddBookEntryForm and EditBookEntryForm update

--- client/src/components/book.components/EditBookEntryForm.jsx
+++ client/src/components/book.components/EditBookEntryForm.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const EditBookEntryForm = () => {
+  const { _id } = useParams();
+  const [book, setBook] = useState({
+    bookRefID: '',
+    title: '',
+    publishYear: '',
+    author: [],
+    genre: [],
+    language: [],
+    shelfLocation: ''
+  })
+
+  useEffect(() => {
+    
+  })
   return (
     <div>EditBookEntryForm</div>
   )
--- client/src/components/book.components/UserBookList.jsx
+++ client/src/components/book.components/UserBookList.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import DataTable from 'react-data-table-component';
+import { useNavigate } from 'react-router-dom';
 
 const UserBookList = () => {
-  return (
-    <div>UserBookList</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState([]);
+  const navigate = useNavigate();
 
-export default UserBookList
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const result = data.filter((data) => {
+      const propertiesToSearch = ['bookRefID', 'title', 'publishYear', 'author', 'genre', 'language', 'shelfLocation'];
+      const lowercasedSearch = search.toLowerCase();
+
+      return propertiesToSearch.some(property =>
+       data[property].toString().toLowerCase().includes(lowercasedSearch)
+      );
+    });
+    setFilter(result);
+  },[search])
+
+  const fetchData = async () => {
+    try {
+      const result = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/books`);
+      const response = await result.json();
+      setData(response.data);
+      setFilter(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleReserveBook = async (_id) => {
+    if (window.confirm('Are you sure you want to reserve this book?')) {
+      alert('Reserved successfully!')
+      navigate('/user/reserve-books')
+      // try {
+      //   fetch(`${import.meta.env.VITE_API_URL}/api/v1/orders/all`, {
+      //     method: 'POST',
+      //   })
+      //   if (response.ok) {
+      //     await response.json();
+      //     fetchData();
+      //     navigate('/user/reserve-books');
+      //   } else {
+      //     const errorMessage = await response.json();
+      //     console.error(errorMessage);
+      //   }
+      // } catch (error) {
+      //   console.error(error);
+      // }
+    } else {}
+  };
+
+  const columns = [
+    {
+      name: 'Book Reference ID',
+      selector: (row) => row.bookRefID,
+      sortable: true,
+    },
+    {
+      name: 'Title',
+      selector: (row) => row.title,
+      sortable: true,
+    },
+    {
+      name: 'Publish Year',
+      selector: (row) => row.publishYear,
+      sortable: true,
+    },
+    {
+      name: 'Author',
+      selector: (row) => row.author,
+      sortable: true,
+    },
+    {
+      name: 'Genre',
+      selector: (row) => row.genre,
+      sortable: true,
+    },
+    {
+      name: 'Language',
+      selector: (row) => row.language,
+      sortable: true,
+    },
+    {
+      name: 'Shelf Location',
+      selector: (row) => row.shelfLocation,
+      sortable: true,
+    },
+    {
+      name: 'Actions',
+      cell: (row) => (
+        <div>
+          <button onClick={handleReserveBook}>Reserve</button>
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <h3>User Book Lists</h3>
+      <DataTable 
+        columns={columns} 
+        data={filter} 
+        selectableRows
+        selectableRowsHighlight
+        highlightOnHover
+        fixedHeader
+        pagination
+        subHeader
+          subHeaderComponent={
+            <input 
+              type='text'
+              className='w-25 form-control'
+              placeholder='Search'
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          }
+      />
+  </div>
+  );
+};
+
+export default UserBookList;
