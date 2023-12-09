9eea765fbd32c82ae39528182eed9e8fb662ae93
daomingley73 <mmotosh@yahoo.com>
2023-12-09 17:35:03 +0800

    feat: reserve button

--- client/src/components/book.components/EditBookEntryForm.jsx
+++ client/src/components/book.components/EditBookEntryForm.jsx
@@ -1,33 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-const EditBookEntryForm = () => {
-  const {_id} = useParams();
-  const [book, setBook] = useState({
+const EditBookEntryForm = ({}) => {
+  const {id} = useParams();
+  const [editBook, setEditBook] = useState({
+    _id: id,
     bookRefID: '',
     title: '',
     publishYear: '',
     author: [],
     genre: [],
     language: [],
-    shelfLocation: ''
+    shelfLocation: '',
   });
   const navigate = useNavigate();
 
-  const handleUpdateBookBtn = async (_id) => {
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_API_URL}/api/v1/books/${id}`)
+    .then(response => {
+      console.log(response)
+      const { bookRefID, title, publishYear, author, genre, language, shelfLocation } = response.data
+      setEditBook({
+        ...editBook,
+        bookRefID,
+        title,
+        publishYear,
+        author,
+        genre,
+        language,
+        shelfLocation,
+      });
+      setEditBook(response.data);
+    })
+    .catch(error => console.log(error))
+  }, [id])
+
+  const handleUpdateBookBtn = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.put(`${import.meta.env.VITE_API_URL}/api/v1/books/${_id}`, book);
-      if (response.ok) {
-        await response.json();
-        alert('Book updated successfully!')
+      const response = await axios.put(`${import.meta.env.VITE_API_URL}/api/v1/books/${id}`, editBook);
+      if (response.status === 200) {
+        alert('Book details updated successfully!')
         navigate('/admin');
       } else {
         console.error(error);
       }
     } catch (error) {
-      console.error(error);
+      console.error('Error updating book:', error);
     }
   }
 
@@ -38,61 +58,61 @@ const EditBookEntryForm = () => {
   return (
     <div>
       <h2>EditBookEntryForm</h2>
-      <form>
+      <form onSubmit={handleUpdateBookBtn}>
         <label>Book Reference ID</label>
         <br />
         <input 
           type='text'
-          value={book.bookRefID}
-          onChange={(e) => setBook({...book, bookRefID: e.target.value})}
+          value={editBook.bookRefID}
+          onChange={(e) => setEditBook({...editBook, bookRefID: e.target.value})}
         />
         <br />
         <label>Title</label>
         <br />
         <input 
           type='text'
-          value={book.title}
-          onChange={(e) => setBook({...book, title: e.target.value})}
+          value={editBook.title}
+          onChange={(e) => setEditBook({...editBook, title: e.target.value})}
         />
         <br />
         <label>Publish Year</label>
         <br />
         <input 
           type='text'
-          value={book.publishYear}
-          onChange={(e) => setBook({...book, publishYear: e.target.value})}
+          value={editBook.publishYear}
+          onChange={(e) => setEditBook({...editBook, publishYear: e.target.value})}
         />
         <br />
         <label>Author</label>
         <br />
         <input 
           type='text'
-          value={book.author}
-          onChange={(e) => setBook({...book, author: e.target.value})}
+          value={editBook.author}
+          onChange={(e) => setEditBook({...editBook, author: e.target.value.split(',')})}
         />
         <br />
         <label>Genre</label>
         <br />
         <input 
           type='text'
-          value={book.genre}
-          onChange={(e) => setBook({...book, genre: e.target.value})}
+          value={editBook.genre}
+          onChange={(e) => setEditBook({...editBook, genre: e.target.value.split(',')})}
         />
         <br />
         <label>Language</label>
         <br />
         <input 
           type='text'
-          value={book.language}
-          onChange={(e) => setBook({...book, language: e.target.value})}
+          value={editBook.language}
+          onChange={(e) => setEditBook({...editBook, language: e.target.value.split(',')})}
         />
         <br />
         <label>Shelf Location</label>
         <br />
         <input 
           type='text'
-          value={book.shelfLocation}
-          onChange={(e) => setBook({...book, shelfLocation: e.target.value})}
+          value={editBook.shelfLocation}
+          onChange={(e) => setEditBook({...editBook, shelfLocation: e.target.value})}
         />
         <br />
         <button type="button" onClick={handleUpdateBookBtn}>Update</button>
