1b3df28fb317fffc2d620878b3836c5ff139a297
jake_v <emelgarjv@gmail.com>
2023-12-09 21:45:55 +0800

    update: fix edit button functionality

--- client/src/pages/AdminPages/EditBook.jsx
+++ client/src/pages/AdminPages/EditBook.jsx
@@ -1,16 +1,13 @@
 import EditBookEntryForm from '../../components/book.components/EditBookEntryForm';
 import AdminHeader from '../../components/header.components/AdminHeader';
-import { useParams } from 'react-router-dom';
 
 const EditBook = () => {
-  const { id } = useParams()
   return (
     <div>
       <AdminHeader />
       <br />
-      <EditBookEntryForm _id={id} />
+      <EditBookEntryForm />
     </div>
   )
 }
-
 export default EditBook
