f390e63bac9f957e094dca7f5bcde231db51419b
jake_v <emelgarjv@gmail.com>
2023-12-10 12:44:50 +0800

    feat: missing page and authorize page styles update

--- client/src/pages/MissingPage.jsx
+++ client/src/pages/MissingPage.jsx
@@ -1,8 +1,15 @@
+import '../index.css';
+
 const MissingPage = () => {
   return (
-    <>
-    <h1>ERROR 404: PAGE NOT FOUND</h1>
-    </>
+    <div className='container square-box d-flex justify-content-center align-items-center flex-column'>
+      <h1 className='text-center text-bg-secondary p-3'>ERROR 404</h1>
+      <h2 className='text-center'>Page not found</h2>
+      <p className='text-center'>The resource requested could not be found on this server!</p>
+    </div>
+      
+   
+    
   );
 }
 export default MissingPage;
