f0aaded91324c9a0a357f33ae908fbd87ec93db4
daomingley73 <mmotosh@yahoo.com>
2023-12-11 19:57:37 +0800

    fix admin booklist responsiveness

--- client/src/components/book.components/AdminBookList.jsx
+++ client/src/components/book.components/AdminBookList.jsx
@@ -71,21 +71,13 @@ const AdminBookList = () => {
       selector: (row) => row.bookRefID,
       sortable: true,
       wrap: true,
-<<<<<<< HEAD
-=======
-      // width: '100px'
->>>>>>> main
     },
     {
       name: 'Title',
       selector: (row) => row.title,
       sortable: true,
       wrap: true,
-<<<<<<< HEAD
       width: '235px',
-=======
-      // width: '23%',
->>>>>>> main
     },
     {
       name: ' Publish Year',
